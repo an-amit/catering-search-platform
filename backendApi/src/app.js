46093cd8c91edc0bf8bb65b7b70f573867cd73e9
an-amit <ambedkaramit176@gmail.com>
2026-06-13 15:54:52 +0530

    add the cors

--- backendApi/src/app.js
+++ backendApi/src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
+const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
+const corsOptions = {
+  origin: ORIGIN,
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
 
 const app = express();
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
